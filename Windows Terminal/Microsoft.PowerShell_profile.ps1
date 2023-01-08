962994b2512212d42185fa48b6d8993a42ce1026
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-01-08 20:08:51 +0700

    Add Windows Terminal

---
+++ Windows Terminal/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,53 @@
+# Import-Module -Name Terminal-Icons
+# Import-Module Az.Tools.Predictor
+
+oh-my-posh init pwsh --config "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\oh-my-posh\themes\emodipt-extend.omp.json" | Invoke-Expression
+
+switch (oh-my-posh get shell) {
+  pwsh {
+    $PSReadLineOptions = @{
+      EditMode                      = "Windows"
+      BellStyle                     = "None"
+      ShowToolTips                  = $true
+      PredictionSource              = "HistoryAndPlugin"
+      HistoryNoDuplicates           = $true
+      PredictionViewStyle           = "ListView"
+      HistorySearchCursorMovesToEnd = $true
+    }
+    
+    Set-PSReadLineOption @PSReadLineOptions
+    Set-PSReadLineOption -Colors @{ emphasis = '#00FF00'; inlinePrediction = 'magenta' }
+  }
+}
+
+function Clear-SavedHistory {
+  [CmdletBinding(ConfirmImpact = 'High', SupportsShouldProcess)]
+  param(
+  )
+
+  $havePSReadline = ($null -ne (Get-Module -EA SilentlyContinue PSReadline))
+  Write-Verbose "PSReadline present: $havePSReadline"
+  # $target = if ($havePSReadline) { "entire command history, including from previous sessions" } else { "command history" } 
+  # if (-not $pscmdlet.ShouldProcess($target)) {
+  #   return
+  # }
+  if ($havePSReadline) {
+    Clear-Host
+    if (Test-Path (Get-PSReadlineOption).HistorySavePath) { 
+      Remove-Item -EA Stop (Get-PSReadlineOption).HistorySavePath 
+      $null = New-Item -Type File -Path (Get-PSReadlineOption).HistorySavePath
+    }
+    Clear-History
+    [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+  }
+  else {
+    Clear-Host
+    $null = [system.reflection.assembly]::loadwithpartialname("System.Windows.Forms")
+    [System.Windows.Forms.SendKeys]::Sendwait('%{F7 2}')
+    Clear-History
+  }
+  Copy-Item -Path "$([Environment]::GetFolderPath("MyDocuments"))\PowerShell\History.txt" -Destination (Get-PSReadLineOption).HistorySavePath -Recurse
+}
+
+# Alias
+Set-Alias -name "pn" -value "pnpm"
---
+++ Windows Terminal/preinstall.ps1
@@ -0,0 +1,38 @@
+if (!([Security.Principal.WindowsPrincipal] `
+            [Security.Principal.WindowsIdentity]::GetCurrent() `
+    ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process pwsh -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    exit
+}
+else {
+    if (!(winget list --name "oh-my-posh")) {
+        winget install JanDeDobbeleer.OhMyPosh -s winget
+    }
+
+    Write-Output "Check Font"
+
+    if (@(New-Object System.Drawing.Text.InstalledFontCollection).Families.Name -match "CaskaydiaCove Nerd Font Mono") {
+        Write-Output "CaskaydiaCove Nerd Font Mono already exists."
+    }
+    else {
+        Write-Output "Not Found CaskaydiaCove Nerd Font Mono"
+        Start-BitsTransfer -Source "https://raw.githubusercontent.com/Faelayis/my-terminal/master/Font/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono.ttf" -Destination "CaskaydiaCove Nerd Font Mono.ttf"
+        & '.\CaskaydiaCove Nerd Font Mono.ttf' -Wait
+    }
+
+    Write-Output ""
+
+    Write-Output "Check Module"
+    
+    $module = Get-Content -Path ".\Module.json" -Raw | ConvertFrom-Json
+    foreach ($i in $module.Module.psobject.properties.name) {
+        if (!(Get-InstalledModule -Name $i)) {
+            Write-Output "Install Module $i"
+            Install-Module -Name @("$i") -Scope AllUsers -Force
+            Write-Output ""
+        }
+    }
+
+
+    Pause
+}
