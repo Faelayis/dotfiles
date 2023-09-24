892fcd86669cbadc9f8c6e397cb7aed3067891b9
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-09-25 03:55:23 +0700

    Update .alias.sh

--- Linux/Fedora/.alias.sh
+++ Linux/Fedora/.alias.sh
@@ -1,22 +1,23 @@
-# Oh My Posh
+# Oh My Posh Configuration
 OhMyPosh_Themes='Documents/GitHub/JanDeDobbeleer/oh-my-posh/themes/emodipt-extend.omp.json'
 eval "$(oh-my-posh init bash --config $([ $EUID != 0 ] && echo "~/$OhMyPosh_Themes" || echo "$OhMyPosh_Themes"))"
 
-# Alias
+# Initialize
 alias sudo='sudo -E'
-alias sudo='sudo '
 alias reload='exec bash --login'
 alias clear='printf "\033c" && reload'
 
-# utility
-alias nf='neofetch'
-
-# packages
+# Packages (NodeJS)
 alias pn='pnpm'
-alias update-flatpak='echo RUN UPDATE "(flatpak)" && sudo flatpak update -y'
-alias update-dnf='echo RUN UPDATE "(dnf)" && sudo dnf clean all && sudo dnf check-update -y && sudo dnf upgrade -y'
-alias update-all='update-dnf; update-ohmyposh; update-flatpak;'
-alias update-ohmyposh='curl -s https://ohmyposh.dev/install.sh | sudo bash -s'
+
+# Packages
+alias update-ohmyposh='echo "RUN UPDATE (Oh My Posh)" && curl -s https://ohmyposh.dev/install.sh | sudo bash -s'
+alias update-flatpak='echo "RUN UPDATE (Flatpak)" && sudo flatpak update -y'
+alias update-dnf='echo "RUN UPDATE (dnf)" && sudo dnf clean all && sudo dnf upgrade -y'
+alias update-all='echo "RUN UPDATE ALL" && update-dnf && update-ohmyposh && update-flatpak'
+
+# Utility
+alias nf='neofetch'
 
 # DNS
 alias dns='sudo gedit /etc/systemd/resolved.conf'
@@ -24,6 +25,5 @@ alias dns='sudo gedit /etc/systemd/resolved.conf'
 # ADB
 alias adb-reload='sudo adb kill-server && sudo adb start-server && adb devices'
 
-# gnome
+# GNOME
 alias gedit="gnome-text-editor"
-
