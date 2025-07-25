61f901b52ec08f12077562d938b8cfbc5cf2c9dd
Faelayis <48393914+Faelayis@users.noreply.github.com>
2025-07-25 23:16:56 +0700

    [Script] Update Data

--- Linux/Fedora/zsh/zinit.sh
+++ Linux/Fedora/zsh/zinit.sh
@@ -8,15 +8,18 @@ zinit light ntnyq/omz-plugin-bun
 zinit light MichaelAquilina/zsh-you-should-use
 
 ## Snippets
+zinit snippet OMZP::bun
 zinit snippet OMZP::dnf
 zinit snippet OMZP::git
 zinit snippet OMZL::git.zsh
+zinit snippet OMZP::copyfile
+zinit snippet OMZP::history
 zinit snippet OMZL::async_prompt.zsh 
 zinit snippet OMZP::command-not-found
 
 ## Config
 # zsh-users/zsh-autosuggestions
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=247'
 ZSH_AUTOSUGGEST_USE_ASYNC=true
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=247'
 
 autoload -Uz compinit && compinit
