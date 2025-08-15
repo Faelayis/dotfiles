e570cb35751aff24351be969b8a022489a33081d
Faelayis <48393914+Faelayis@users.noreply.github.com>
2025-08-15 08:53:14 +0700

    Add force option to akmods command in setup script

--- external/setup.sh
+++ external/setup.sh
@@ -2,6 +2,7 @@ sudo dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx li
 sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda-libs -y
 
 modinfo -F version nvidia
+sudo akmods --force
 
 echo "blacklist nouveau" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
 echo "options nouveau modset=0" | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf
